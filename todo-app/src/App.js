4bdde1546a9defb662cfa4d088db4b3f4bd39a6a
Orosz Dávid <orosz.david@sonrisa.hu>
2020-06-03 10:33:34 +0200

    add demo solution

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,26 +1,118 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  
+  constructor (props) {
+    super(props);
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.markTodoDone = this.markTodoDone.bind(this);
+
+    this.state = {todoItems: []};
+  }
+  addItem(todoItem) {
+    this.state.todoItems.unshift({
+      index: this.state.todoItems.length+1, 
+      value: todoItem.newItemValue, 
+      done: false
+    });
+    this.setState({todoItems: this.state.todoItems});
+  }
+  removeItem (itemIndex) {
+    this.state.todoItems.splice(itemIndex, 1);
+    this.setState({todoItems: this.state.todoItems});
+  }
+  markTodoDone(itemIndex) {
+    var todo = this.state.todoItems[itemIndex];
+    this.state.todoItems.splice(itemIndex, 1);
+    todo.done = !todo.done;
+    todo.done ? this.state.todoItems.push(todo) : this.state.todoItems.unshift(todo);
+    this.setState({todoItems: this.state.todoItems});  
+  }
+  render() {
+    return (
+      <div id="main">
+        <TodoHeader />
+        <TodoList items={this.state.todoItems} removeItem={this.removeItem} markTodoDone={this.markTodoDone}/>
+        <TodoForm addItem={this.addItem} />
+      </div>
+    );
+  }
 }
 
-export default App;
+class TodoList extends React.Component {
+  render () {
+    var items = this.props.items.map((item, index) => {
+      return (
+        <TodoListItem key={index} item={item} index={index} removeItem={this.props.removeItem} markTodoDone={this.props.markTodoDone} />
+      );
+    });
+    return (
+      <ul className="list-group"> {items} </ul>
+    );
+  }
+}
+  
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClickClose = this.onClickClose.bind(this);
+    this.onClickDone = this.onClickDone.bind(this);
+  }
+  onClickClose() {
+    var index = parseInt(this.props.index);
+    this.props.removeItem(index);
+  }
+  onClickDone() {
+    var index = parseInt(this.props.index);
+    this.props.markTodoDone(index);
+  }
+  render () {
+    var todoClass = this.props.item.done ? 
+        "done" : "undone";
+    return(
+      <li className="list-group-item ">
+        <div className={todoClass}>
+          <span className="glyphicon glyphicon-ok icon" aria-hidden="true" onClick={this.onClickDone}></span>
+          {this.props.item.value}
+          <button type="button" className="close" onClick={this.onClickClose}>&times;</button>
+        </div>
+      </li>     
+    );
+  }
+}
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  componentDidMount() {
+    this.refs.itemName.focus();
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    var newItemValue = this.refs.itemName.value;
+    
+    if(newItemValue) {
+      this.props.addItem({newItemValue});
+      this.refs.form.reset();
+    }
+  }
+  render () {
+    return (
+      <form ref="form" onSubmit={this.onSubmit} className="form-inline marginTop">
+        <input type="text" ref="itemName" className="form-control" placeholder="add a new todo..."/>
+        <button type="submit" class="btn btn-primary">Add</button> 
+      </form>
+    );   
+  }
+}
+  
+class TodoHeader extends React.Component {
+  render () {
+    return <h1>Todo list</h1>;
+  }
+}
